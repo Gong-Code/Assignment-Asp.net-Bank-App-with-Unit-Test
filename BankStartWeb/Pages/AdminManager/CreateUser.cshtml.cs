6537d9c28dbf3d725942c8e775d0bdf9b570adba
Gong-Code <gong.moonphruk@gmail.com>
2022-05-11 15:01:33 +0200

    added css

--- BankStartWeb/Pages/AdminManager/CreateUser.cshtml.cs
+++ BankStartWeb/Pages/AdminManager/CreateUser.cshtml.cs
@@ -24,8 +24,7 @@ namespace BankStartWeb.Pages.AdminManager
         public string Email { get; set; }
         [DataType(DataType.Password)] public string Password { get; set; }
         public List<string> Roles { get; set; }
-        public List<SelectListItem> AllRoles { get; set; } 
-
+        public List<SelectListItem> AllRoles { get; set; }
 
         public void OnGet()
         {
